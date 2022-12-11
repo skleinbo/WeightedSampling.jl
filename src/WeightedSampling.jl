6c2e11d49a6e762a5857a7a42f2ba076fe1dfac6
Stephan Kleinbölting <skleinbo@thp.uni-koeln.de>
2022-12-11 10:01:27 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/WeightedSampling.jl
@@ -0,0 +1,5 @@
+module WeightedSampling
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WeightedSampling
+using Test
+
+@testset "WeightedSampling.jl" begin
+    # Write your tests here.
+end
